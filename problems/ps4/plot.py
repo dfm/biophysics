68ae2a97e73fd324be968983b5ea965ba5095a30
Dan F-M <danfm@nyu.edu>
2014-11-02 21:11:51 -0500

    working on the midterm

--- problems/ps4/plot.py
+++ problems/ps4/plot.py
@@ -11,20 +11,23 @@ def h(x, y):
     return x*x + x*y - 2*y*y
 
 
+linestyles = ["-", "--", ":", "-."]
 fig = pl.figure(figsize=(10, 5))
 
 x = np.linspace(0, 1, 5000)
-y = np.linspace(0, 1, 5)
+y = np.linspace(0, 1, 4)
 ax = fig.add_subplot(121)
-[ax.plot(x, h(x, y0), label="$y={0:.2f}$".format(y0), lw=2) for y0 in y]
+[ax.plot(x, h(x, y0), label="$y={0:.2f}$".format(y0), lw=2, ls=ls)
+ for y0, ls in zip(y, linestyles)]
 ax.legend(fontsize=10, loc=2)
 ax.set_xlabel("$x$")
 ax.set_ylabel(r"$h(x,\,y)$")
 
 y = np.linspace(0, 1, 5000)
-x = np.linspace(0, 1, 5)
+x = np.linspace(0, 1, 4)
 ax = fig.add_subplot(122)
-[ax.plot(y, h(x0, y), label="$x={0:.2f}$".format(x0), lw=2) for x0 in x]
+[ax.plot(y, h(x0, y), label="$x={0:.2f}$".format(x0), lw=2, ls=ls)
+ for x0, ls in zip(x, linestyles)]
 ax.legend(fontsize=10, loc=3)
 ax.set_yticklabels([])
 ax.set_xlabel("$y$")
